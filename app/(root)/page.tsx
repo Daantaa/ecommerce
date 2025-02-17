7729c159e7529df0a402232b47a6d1191e859d67
Daantaa <d2nutn@gmail.com>
2025-02-17 11:38:37 +0200

    Environment Setup and Clerk Authentication (Admin)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+    return (
+      <div className="p-5">
+        <UserButton />
+      </div>
+    );
+  }
+  
+  export default SetupPage;
